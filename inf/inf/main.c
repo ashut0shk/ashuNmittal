780facdf013f221d112b6257da7ab3a08a69c0a0
Ashutosh K <ashutoshk@infekted.local>
2015-11-09 01:44:28 +0530

    added

---
+++ inf/inf/main.c
@@ -0,0 +1,56 @@
+	//
+	//  geeksforgeeks.c
+	//  test
+	//
+	//  Created by Ashutosh K on 11/8/15.
+	//  Copyright © 2015 Ashutosh K. All rights reserved.
+	//
+	//Condition 1 If the number have any other digits tahn the given reversible the it cannot be fancy
+	//if all the digits are reversible, then replace all 6 with 9 and check if the number is same as input one, if yes then only ythe number is fancy, else not fancy.
+#include <stdio.h>
+#include <math.h>
+//using namespace std;
+int rotateBy180(const int input){
+	int binput = input;
+	int actA = {6,9,1,0,8};
+	int mapA = {9,6,1,0,8};
+	
+	int nDigit = 0;
+	unsigned int i = 0;
+	nDigit = floor(log10(abs(binput))) + 1;
+	int * arrayp = (int *)malloc(sizeof(int)*nDigit);
+	int * arrayq = (int *)malloc(sizeof(int)*nDigit);
+	short int oneDig;
+	do {
+		oneDig = binput % 10;
+		if(oneDig != 6 && oneDig != 9 && oneDig != 1 && oneDig != 0 && oneDig != 8){
+			return 0;
+		}
+		arrayp[i] = oneDig;
+		if(oneDig == 6){
+			arrayq[i] = 9;
+		}else if(oneDig == 9){
+			arrayq[i] = 6;
+		}else{
+			arrayq[i] = oneDig;
+		}
+		binput /= 10;
+	} while (binput != 0);
+	for (i=0; i<nDigit; ++i) {
+		if(arrayp[i] != arrayq[i]){
+			return 0;
+		}
+	}
+	return 1;
+}
+int main(){
+		//180 degree rotations of 6, 9, 1, 0 and 8 are 9, 6, 1, 0 and 8 respectively
+	
+	int input = 69;
+	printf("program started with %d\n", rand());
+	int revA = rotateBy180(input);
+	char * str= (revA == 1)?" Fancy." : " Not Fancy.";
+	printf("Given number is %s", str);
+	printf("\n\nprogram ended with %d\n", rand());
+	
+}
---
+++ inf/inf/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  inf
+//
+//  Created by Ashutosh K on 11/9/15.
+//  Copyright © 2015 Ashutosh K. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
