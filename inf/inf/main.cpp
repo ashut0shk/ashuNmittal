a25130d1142ee3af45f37c5535206873fafac417
Ashutosh K <ashutoshk@infekted.local>
2015-11-09 01:38:14 +0530

    added

---
+++ inf/inf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inf
+//
+//  Created by Ashutosh K on 11/9/15.
+//  Copyright © 2015 Ashutosh K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
